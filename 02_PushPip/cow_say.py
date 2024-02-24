bfeb2a1b7752e9c38eba75e5090747ef9b574011
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej-2.local>
2024-02-24 18:55:01 +0300

    add nothing do argparse

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,5 +3,13 @@ from cowsay import cowsay
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("-e", )
+parser.add_argument("-f", )
+parser.add_argument("-l", )
+parser.add_argument("-n", )
+parser.add_argument("-T", )
+parser.add_argument("-W", )
+parser.add_argument("-bdgpstwy", )
+
 parser.parse_args()
 
