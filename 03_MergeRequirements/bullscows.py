ec58a58c4c4ca66632ce5015ca37dfbee32fd41b
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej-2.local>
2024-03-04 19:30:24 +0300

    add skeleton

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def bullscows(guess: str, secret: str) -> (int, int):
+    pass
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    pass
