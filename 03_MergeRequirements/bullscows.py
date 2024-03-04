138d3ff8737aaba22868a56465dc608089ff296c
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej-2.local>
2024-03-04 20:01:34 +0300

    complete the function bullcows

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -2,7 +2,25 @@ import sys
 
 
 def bullscows(guess: str, secret: str) -> (int, int):
-    pass
+    cow = 0
+    s_m = dict()
+
+    for i in secret:
+        s_m.setdefault(i, 0)
+        s_m[i] += 1
+    
+    for i in guess:
+        if i in s_m.keys() and s_m[i] > 0:
+            cow += 1
+            s_m[i] -= 1
+
+    bull = 0
+
+    for i in range(len(guess)):
+        if guess[i] == secret[i]:
+            bull += 1
+
+    return (bull, cow)
 
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
@@ -10,4 +28,4 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
 
 
 if __name__ == "__main__":
-    pass
+    print(bullscows("ропот", "полип"))
